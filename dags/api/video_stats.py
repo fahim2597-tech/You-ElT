f81ce45e83851dbae62cfad9ebfa4956a188699a
Md .Fahim Faysal Rabby <fahimudemy66@gmail.com>
2026-07-22 18:51:43 +0600

    Refactor video stats API functions

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -52,6 +52,7 @@ def get_playlist_id():
         
 
 @task
+API_KEY = Variable.get("API_KEY")
 def get_video_ids(playlistId):
 
     video_ids = []
@@ -91,6 +92,7 @@ def get_video_ids(playlistId):
 
 
 @task
+API_KEY = Variable.get("API_KEY")
 def extract_video_data(video_ids):
 
     extracted_data = []
